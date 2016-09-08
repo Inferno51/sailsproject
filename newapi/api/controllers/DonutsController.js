a609ae0a27e89b89bceb9224b0da01a735d0c514
Jon Pack <jon.pack@me.com>
2016-09-08 09:49:43 -0400

    First commit, created simple sails project

---
+++ newapi/api/controllers/DonutsController.js
@@ -0,0 +1,11 @@
+/**
+ * DonutsController
+ *
+ * @description :: Server-side logic for managing donuts
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
